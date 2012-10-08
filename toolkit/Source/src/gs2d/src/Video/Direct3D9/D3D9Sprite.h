5b1ea7138543862471068e581f2fd6b3563d849f
Andre Santee <andre.santee@gmail.com>
2012-10-08 16:38:25 -0400

    File prefixes removed

---
+++ toolkit/Source/src/gs2d/src/Video/Direct3D9/D3D9Sprite.h
@@ -0,0 +1,143 @@
+/*--------------------------------------------------------------------------------------
+ Ethanon Engine (C) Copyright 2008-2012 Andre Santee
+ http://www.asantee.net/ethanon/
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this
+	software and associated documentation files (the "Software"), to deal in the
+	Software without restriction, including without limitation the rights to use, copy,
+	modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+	and to permit persons to whom the Software is furnished to do so, subject to the
+	following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+	PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+	CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+	OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--------------------------------------------------------------------------------------*/
+
+#ifndef GS2D_D3D9_SPRITE_H_
+#define GS2D_D3D9_SPRITE_H_
+
+#include "D3D9Video.h"
+
+namespace gs2d {
+
+/**
+ * \brief Implements a Direct3D 9 Sprite object
+ */
+class D3D9Sprite : public Sprite
+{
+	bool LoadSprite(
+		VideoWeakPtr video,
+		GS_BYTE *pBuffer,
+		const unsigned int bufferLength,
+		Color mask = constant::ZERO,
+		const unsigned int width = 0,
+		const unsigned int height = 0);
+
+	bool LoadSprite(
+		VideoWeakPtr video,
+		const std::wstring& fileName,
+		Color mask = constant::ZERO,
+		const unsigned int width = 0,
+		const unsigned int height = 0);
+
+	bool CreateRenderTarget(
+		VideoWeakPtr video,
+		const unsigned int width,
+		const unsigned int height,
+		const GS_TARGET_FORMAT format = GSTF_DEFAULT);
+
+	VideoWeakPtr m_video;
+	TexturePtr m_texture;
+	math::Vector2 m_scroll;
+	math::Vector2 m_multiply;
+	math::Vector2 m_size;
+	GS_TARGET_FORMAT m_targetFormat;
+	bool m_flipX, m_flipY;
+	Sprite::TYPE m_type;
+	IDirect3DTexture9 *m_pBackupTexture;
+
+	IDirect3DTexture9 *m_pTexture;
+	IDirect3DDevice9 *m_pDevice;
+	D3D9VideoInfoPtr m_pVideoInfo;
+
+	void Init();
+	bool GetInternalData();
+	void OnLostDevice();
+	void RecoverFromBackup();
+
+public:
+	D3D9Sprite();
+	~D3D9Sprite();
+
+	bool Draw(
+		const math::Vector2& v2Pos,
+		const Color& color = constant::WHITE,
+		const float angle = 0.0f,
+		const math::Vector2& v2Scale = math::Vector2(1.0f,1.0f));
+
+	bool DrawShaped(
+		const math::Vector2& v2Pos,
+		const math::Vector2& v2Size,
+		const Color& color0,
+		const Color& color1,
+		const Color& color2,
+		const Color& color3,
+		const float angle = 0.0f);
+
+	bool SaveBitmap(const wchar_t *wcsName, const GS_BITMAP_FORMAT fmt, math::Rect2D *pRect = 0);
+
+	bool DrawShapedFast(const math::Vector2 &v2Pos, const math::Vector2 &v2Size, const Color& color);
+
+	bool DrawOptimal(
+		const math::Vector2& v2Pos,
+		const Color& color = constant::WHITE,
+		const float angle = 0.0f,
+		const math::Vector2& v2Size = math::Vector2(-1,-1));
+
+	void BeginFastRendering();
+	void EndFastRendering();
+
+	TextureWeakPtr GetTexture();
+
+	Texture::PROFILE GetProfile() const;
+	math::Vector2i GetBitmapSize() const;
+	math::Vector2 GetBitmapSizeF() const;
+
+	void FlipX(const bool flip);
+	void FlipY(const bool flip);
+	void FlipX();
+	void FlipY();
+	bool GetFlipX() const;
+	bool GetFlipY() const;
+
+	void SetScroll(const math::Vector2 &v2Scroll);
+	math::Vector2 GetScroll() const;
+
+	void SetMultiply(const math::Vector2 &v2Multiply);
+	math::Vector2 GetMultiply() const;
+
+	TYPE GetType() const;
+	boost::any GetTextureObject();
+
+	void GenerateBackup();
+	bool SetAsTexture(const unsigned int passIdx);
+
+	void SetSpriteDensityValue(const float value);
+	float GetSpriteDensityValue() const;
+
+	math::Vector2 GetTextureSize() const;
+};
+
+typedef boost::shared_ptr<D3D9Sprite> D3D9SpritePtr;
+typedef boost::weak_ptr<D3D9Sprite> D3D9SpriteWeakPtr;
+
+} // namespace gs2d
+
+#endif
---
+++ toolkit/Source/src/gs2d/src/Video/Direct3D9/D3D9Texture.h
@@ -0,0 +1,76 @@
+/*--------------------------------------------------------------------------------------
+ Ethanon Engine (C) Copyright 2008-2012 Andre Santee
+ http://www.asantee.net/ethanon/
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this
+	software and associated documentation files (the "Software"), to deal in the
+	Software without restriction, including without limitation the rights to use, copy,
+	modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+	and to permit persons to whom the Software is furnished to do so, subject to the
+	following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+	PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+	CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+	OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--------------------------------------------------------------------------------------*/
+
+#ifndef GS2D_D3D9_TEXTURE_H_
+#define GS2D_D3D9_TEXTURE_H_
+
+#include "../../Texture.h"
+#include <d3dx9.h>
+
+namespace gs2d {
+
+/**
+ * \brief Implements a Direct3D 9 Texture object
+ */
+class D3D9Texture : public Texture
+{
+	IDirect3DTexture9 *m_pTexture;
+	IDirect3DDevice9 *m_pDevice;
+	VideoWeakPtr m_video;
+	PROFILE m_profile;
+	TYPE  m_texType;
+
+	bool GetInternalData();
+
+public:
+	D3D9Texture();
+	~D3D9Texture();
+
+	bool SetTexture(const unsigned int passIdx = 0);
+	PROFILE GetProfile() const;
+	TYPE GetTextureType() const;
+	boost::any GetTextureObject();
+	math::Vector2 GetBitmapSize() const;
+
+protected:
+	bool CreateRenderTarget(
+		VideoWeakPtr video, const unsigned int width, const unsigned int height, const GS_TARGET_FORMAT fmt
+	);
+	bool LoadTexture(
+		VideoWeakPtr video,
+		const std::wstring& fileName, Color mask,
+			const unsigned int width = 0, const unsigned int height = 0,
+			const unsigned int nMipMaps = 0
+	);
+	virtual bool LoadTexture(
+		VideoWeakPtr video,
+		const void * pBuffer, Color mask,
+			const unsigned int width, const unsigned int height,
+			const unsigned int nMipMaps,
+			const unsigned int bufferLength
+	);
+
+};
+
+} // namespace gs2d
+
+#endif
