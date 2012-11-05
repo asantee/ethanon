b9a047259544ca65cf08126654d3f1ef1adb8ec3
André Santee <andre.santee@gmail.com>
2012-11-04 22:38:56 -0300

    Box2D and AngelScript projects linked

---
+++ toolkit/Source/src/box2d/Build/xcode/box2d/box2d.h
@@ -0,0 +1,13 @@
+//
+//  box2d.h
+//  box2d
+//
+//  Created by Andre Santee on 11/4/12.
+//  Copyright (c) 2012 Asantee Games. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface box2d : NSObject
+
+@end
---
+++ toolkit/Source/src/box2d/Build/xcode/box2d/box2d.m
@@ -0,0 +1,13 @@
+//
+//  box2d.m
+//  box2d
+//
+//  Created by Andre Santee on 11/4/12.
+//  Copyright (c) 2012 Asantee Games. All rights reserved.
+//
+
+#import "box2d.h"
+
+@implementation box2d
+
+@end
