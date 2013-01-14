818cea14e72de5b2f6a3f766649f34ed4ca39c8f
Pablo Marcos Oltra <pablo.marcos.oltra@gmail.com>
2013-01-14 20:30:08 +0100

    AngelScript project for Linux, using Eclipse to generate Makefiles

---
+++ toolkit/Source/src/angelscript/projects/eclipse/Release/source/subdir.mk
@@ -0,0 +1,394 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_atomic.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_builder.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_bytecode.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_arm.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_mips.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_ppc.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_ppc_64.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_sh4.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_x64_gcc.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_x64_mingw.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_x64_msvc.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_x86.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_xenon.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_compiler.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_configgroup.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_context.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_datatype.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_gc.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_generic.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_globalproperty.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_memory.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_module.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_objecttype.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_outputbuffer.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_parser.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_restore.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_scriptcode.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_scriptengine.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_scriptfunction.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_scriptnode.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_scriptobject.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_string.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_string_util.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_thread.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_tokenizer.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_typeinfo.cpp \
+/media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_variablescope.cpp 
+
+OBJS += \
+./source/as_atomic.o \
+./source/as_builder.o \
+./source/as_bytecode.o \
+./source/as_callfunc.o \
+./source/as_callfunc_arm.o \
+./source/as_callfunc_mips.o \
+./source/as_callfunc_ppc.o \
+./source/as_callfunc_ppc_64.o \
+./source/as_callfunc_sh4.o \
+./source/as_callfunc_x64_gcc.o \
+./source/as_callfunc_x64_mingw.o \
+./source/as_callfunc_x64_msvc.o \
+./source/as_callfunc_x86.o \
+./source/as_callfunc_xenon.o \
+./source/as_compiler.o \
+./source/as_configgroup.o \
+./source/as_context.o \
+./source/as_datatype.o \
+./source/as_gc.o \
+./source/as_generic.o \
+./source/as_globalproperty.o \
+./source/as_memory.o \
+./source/as_module.o \
+./source/as_objecttype.o \
+./source/as_outputbuffer.o \
+./source/as_parser.o \
+./source/as_restore.o \
+./source/as_scriptcode.o \
+./source/as_scriptengine.o \
+./source/as_scriptfunction.o \
+./source/as_scriptnode.o \
+./source/as_scriptobject.o \
+./source/as_string.o \
+./source/as_string_util.o \
+./source/as_thread.o \
+./source/as_tokenizer.o \
+./source/as_typeinfo.o \
+./source/as_variablescope.o 
+
+CPP_DEPS += \
+./source/as_atomic.d \
+./source/as_builder.d \
+./source/as_bytecode.d \
+./source/as_callfunc.d \
+./source/as_callfunc_arm.d \
+./source/as_callfunc_mips.d \
+./source/as_callfunc_ppc.d \
+./source/as_callfunc_ppc_64.d \
+./source/as_callfunc_sh4.d \
+./source/as_callfunc_x64_gcc.d \
+./source/as_callfunc_x64_mingw.d \
+./source/as_callfunc_x64_msvc.d \
+./source/as_callfunc_x86.d \
+./source/as_callfunc_xenon.d \
+./source/as_compiler.d \
+./source/as_configgroup.d \
+./source/as_context.d \
+./source/as_datatype.d \
+./source/as_gc.d \
+./source/as_generic.d \
+./source/as_globalproperty.d \
+./source/as_memory.d \
+./source/as_module.d \
+./source/as_objecttype.d \
+./source/as_outputbuffer.d \
+./source/as_parser.d \
+./source/as_restore.d \
+./source/as_scriptcode.d \
+./source/as_scriptengine.d \
+./source/as_scriptfunction.d \
+./source/as_scriptnode.d \
+./source/as_scriptobject.d \
+./source/as_string.d \
+./source/as_string_util.d \
+./source/as_thread.d \
+./source/as_tokenizer.d \
+./source/as_typeinfo.d \
+./source/as_variablescope.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+source/as_atomic.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_atomic.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_builder.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_builder.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_bytecode.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_bytecode.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_callfunc.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_callfunc_arm.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_arm.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_callfunc_mips.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_mips.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_callfunc_ppc.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_ppc.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_callfunc_ppc_64.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_ppc_64.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_callfunc_sh4.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_sh4.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_callfunc_x64_gcc.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_x64_gcc.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_callfunc_x64_mingw.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_x64_mingw.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_callfunc_x64_msvc.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_x64_msvc.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_callfunc_x86.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_x86.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_callfunc_xenon.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_callfunc_xenon.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_compiler.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_compiler.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_configgroup.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_configgroup.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_context.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_context.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_datatype.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_datatype.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_gc.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_gc.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_generic.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_generic.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_globalproperty.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_globalproperty.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_memory.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_memory.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_module.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_module.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_objecttype.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_objecttype.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_outputbuffer.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_outputbuffer.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_parser.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_parser.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_restore.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_restore.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_scriptcode.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_scriptcode.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_scriptengine.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_scriptengine.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_scriptfunction.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_scriptfunction.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_scriptnode.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_scriptnode.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_scriptobject.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_scriptobject.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_string.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_string.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_string_util.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_string_util.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_thread.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_thread.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_tokenizer.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_tokenizer.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_typeinfo.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_typeinfo.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+source/as_variablescope.o: /media/pablo/HD1/Programacion/ethanon/toolkit/Source/src/angelscript/source/as_variablescope.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -I"/../../../source" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
