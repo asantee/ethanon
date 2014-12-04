155c8705e166ee1b4f900d8abf8cf0ab1608f0c2
André Santee <andre.santee@gmail.com>
2014-12-04 16:44:22 -0300

    New iOS project base

---
+++ toolkit/Source/projects/xcode/iOS/Application/Application/Application.h
@@ -0,0 +1,13 @@
+//
+//  Application.h
+//  Application
+//
+//  Created by Andre Santee on 4/12/14.
+//  Copyright (c) 2014 Asantee Games. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Application : NSObject
+
+@end
---
+++ toolkit/Source/projects/xcode/iOS/Application/Application/Application.m
@@ -0,0 +1,13 @@
+//
+//  Application.m
+//  Application
+//
+//  Created by Andre Santee on 4/12/14.
+//  Copyright (c) 2014 Asantee Games. All rights reserved.
+//
+
+#import "Application.h"
+
+@implementation Application
+
+@end
