42d117cf4274132fe655d7c1eb0995f05c1c6d72
André Santee <andre.santee@gmail.com>
2014-12-09 17:29:28 -0300

    Old GS2D standalone android project removed

--- toolkit/Source/src/gs2d/projects/Android/GS2D/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_STL := gnustl_static
-APP_CPPFLAGS += -fexceptions
-APP_CPPFLAGS += -frtti
--- toolkit/Source/src/gs2d/projects/Android/GS2D/src/net/asantee/gs2d/io/CommandForwarder.java
+++
@@ -1,5 +0,0 @@
-package net.asantee.gs2d.io;
-
-public interface CommandForwarder {
-	public String getCommands();
-}
