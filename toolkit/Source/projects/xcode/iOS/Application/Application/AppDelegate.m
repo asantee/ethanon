407f96cd018658da244f96527378e7a64e476f09
André Santee <andre.santee@gmail.com>
2014-12-10 12:07:53 -0300

    AppDelegate moved to libApplication; Pause and resuming music according to app cycle

--- toolkit/Source/projects/xcode/iOS/Application/Application/AppDelegate.m
+++ toolkit/Source/projects/xcode/iOS/Application/Application/AppDelegate.m
@@ -8,12 +8,26 @@
 
 #import "AppDelegate.h"
 
+#import "CocosDenshion/SimpleAudioEngine.h"
+
 @interface AppDelegate ()
+{
+	BOOL wasBackgroundMusicPlaying;
+}
 
 @end
 
 @implementation AppDelegate
 
+- (id)init 
+{
+	self = [super init];
+	if (self) 
+	{
+		wasBackgroundMusicPlaying = FALSE;
+	}
+	return self;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 	// Override point for customization after application launch.
@@ -23,6 +37,12 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
 	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
+	wasBackgroundMusicPlaying = [[SimpleAudioEngine sharedEngine] isBackgroundMusicPlaying];
+	if (wasBackgroundMusicPlaying)
+	{
+		[[SimpleAudioEngine sharedEngine] pauseBackgroundMusic];
+	}
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -36,6 +56,11 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
 	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+	
+	if (wasBackgroundMusicPlaying)
+	{
+		[[SimpleAudioEngine sharedEngine] resumeBackgroundMusic];
+	}
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
--- toolkit/Source/projects/xcode/iOS/iOSBase/iOSBase/main.m
+++ toolkit/Source/projects/xcode/iOS/iOSBase/iOSBase/main.m
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+
+#import "Application/AppDelegate.h"
 
 int main(int argc, char * argv[]) {
 	@autoreleasepool {
