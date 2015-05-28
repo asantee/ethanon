3fc9d93b51c70857f26c4728e2b546f151072af4
André Santee <andre.santee@gmail.com>
2015-05-28 02:05:08 -0400

    Building only for architectures that are actually supported

--- toolkit/Source/src/gs2d/projects/Android/Ethanon/jni/Application.mk
+++ toolkit/Source/src/gs2d/projects/Android/Ethanon/jni/Application.mk
@@ -2,4 +2,4 @@ APP_PLATFORM := android-8
 APP_STL := gnustl_static
 APP_CPPFLAGS += -fexceptions
 APP_CPPFLAGS += -frtti
-APP_ABI := armeabi armeabi-v7a x86 arm64-v8a x86_64 mips mips64
+APP_ABI := armeabi armeabi-v7a x86
