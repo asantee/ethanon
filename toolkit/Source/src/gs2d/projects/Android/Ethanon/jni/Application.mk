964824931e9fc4bac61921324a4985f38b713959
André Santee <andre.santee@gmail.com>
2019-08-19 11:45:03 -0400

    Update Application.mk

--- toolkit/Source/src/gs2d/projects/Android/Ethanon/jni/Application.mk
+++ toolkit/Source/src/gs2d/projects/Android/Ethanon/jni/Application.mk
@@ -2,4 +2,4 @@ APP_PLATFORM := android-16
 APP_STL := c++_static
 APP_CPPFLAGS += -fexceptions
 APP_CPPFLAGS += -frtti
-APP_ABI := armeabi-v7a x86 arm64-v8a
+APP_ABI := armeabi-v7a x86 arm64-v8a x86_64
