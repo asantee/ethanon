d5213cfa7588490c6cd15a2de12eae37952cadda
André Santee <andre.santee@gmail.com>
2021-06-15 17:59:44 -0400

    More Neon optimizations to Vector2

--- toolkit/Source/src/gs2d/src/Math/Vector2.cpp
+++ toolkit/Source/src/gs2d/src/Math/Vector2.cpp
@@ -11,6 +11,36 @@
 namespace gs2d {
 namespace math {
 
+#if defined(__ARM_NEON__)
+	float sqrtest(float a)
+	{
+		// need to "transfer" or "convert" the scalar input
+		// to a vector of two
+		// - optimally we would not need an instruction for that
+		// but we would just let the processor calculate the instruction
+		// for all the lanes in the register
+		float32x2_t a2 = vdup_n_f32(a);
+
+		// next we create a mask that is all ones for the legal
+		// domain of 1/sqrt(x)
+		auto is_legal = vreinterpret_f32_u32(vcgt_f32(a2, vdup_n_f32(0.0f)));
+
+		// calculate two reciprocal estimates in parallel
+		float32x2_t a2est = vrsqrte_f32(a2);
+
+		// we need to mask the result, so that effectively
+		// all non-legal values of a2est are zeroed
+		a2est = vand_u32(is_legal, a2est);
+
+		// x * 1/sqrt(x) == sqrt(x)
+		a2 = vmul_f32(a2, a2est);
+
+		// finally we get only the zero lane of the result
+		// discarding the other half
+		return vget_lane_f32(a2, 0);
+	}
+#endif
+
 Vector2::Vector2()
 {
 	x = y = 0.0f;
@@ -34,7 +64,11 @@ Vector2::Vector2(const float& vx, const float& vy)
 
 float Vector2::Length() const
 {
-	return sqrtf(SquaredLength());
+	#if defined(__ARM_NEON__)
+		return sqrtest(SquaredLength());
+	#else
+		return sqrtf(SquaredLength());
+	#endif
 }
 
 float Vector2::SquaredLength() const
@@ -44,7 +78,7 @@ float Vector2::SquaredLength() const
 		v = vpadd_f32(v, v);
 		return (vget_lane_f32(v, 0));
 	#else
-		return (x * x + y * y);
+		return ((x * x) + (y * y));
 	#endif
 }
 
@@ -158,26 +192,50 @@ bool Vector2::operator != (const Vector2& v) const
 
 Vector2& Vector2::operator += (const Vector2& v)
 {
-	*this = Vector2(x, y) + v;
-	return *this;
+	#if defined(__ARM_NEON__)
+		*(float32x2_t*)this = vadd_f32(*(float32x2_t*)&x, *(float32x2_t*)&v);
+		return *this;
+	#else
+		*this = Vector2(x, y) + v;
+		return *this;
+	#endif
 }
 
 Vector2& Vector2::operator -= (const Vector2& v)
 {
-	*this = Vector2(x, y) - v;
-	return *this;
+	#if defined(__ARM_NEON__)
+		*(float32x2_t*)this = vsub_f32(*(float32x2_t*)&x, *(float32x2_t*)&v);
+		return *this;
+	#else
+		*this = Vector2(x, y) - v;
+		return *this;
+	#endif
 }
 
 Vector2& Vector2::operator *= (const float f)
 {
-	*this = Vector2(x, y) * f;
-	return *this;
+	#if defined(__ARM_NEON__)
+		*(float32x2_t*)this = vmul_f32(*(float32x2_t*)&x, vdup_n_f32((float32_t)f));
+		return *this;
+	#else
+		*this = Vector2(x, y) * f;
+		return *this;
+	#endif
 }
 
 Vector2& Vector2::operator /= (const float f)
 {
-	*this = Vector2(x, y) / f;
-	return *this;
+	#if defined(__ARM_NEON__)
+		float32x2_t values = vdup_n_f32((float32_t)f);
+		float32x2_t estimate = vrecpe_f32(values);
+		estimate = vmul_f32(vrecps_f32(values, estimate), estimate);
+		estimate = vmul_f32(vrecps_f32(values, estimate), estimate);
+		*(float32x2_t*)this = vmul_f32(*(float32x2_t *)&x, estimate);
+		return *this;
+	#else
+		*this = Vector2(x, y) / f;
+		return *this;
+	#endif
 }
 
 Vector2i Vector2::ToVector2i() const
@@ -193,7 +251,7 @@ Vector2 Vector2::ToVector2(const Vector2i& v)
 float Vector2::Distance(const Vector2& a, const Vector2& b)
 {
 	const Vector2 diff = b - a;
-	return sqrtf(DP2(diff, diff));
+	return diff.Length();
 }
 
 float Vector2::GetAngle(const Vector2& v2)
@@ -204,15 +262,21 @@ float Vector2::GetAngle(const Vector2& v2)
 
 float Vector2::DP2(const Vector2& a, const Vector2& b)
 {
-    return ((a.x * b.x) + (a.y * b.y));
+	#if defined(__ARM_NEON__)
+		float32x2_t v = vmul_f32(*(float32x2_t*)&a, *(float32x2_t *)&b);
+		v = vpadd_f32(v, v);
+		return (vget_lane_f32(v, 0));
+	#else
+	    return ((a.x * b.x) + (a.y * b.y));
+	#endif
 }
 
 Vector2 Vector2::Normalize(const Vector2& v)
 {
-    const float a = sqrtf(DP2(v, v));
+    const float a = v.Length();
     if (a <= 0.0f)
         return Vector2(0, 0);
-    return Vector2(v.x / a, v.y / a);
+    return (v / a);
 }
 
 Vector2 Vector2::Vector2Min(const Vector2& a, const Vector2& b)
