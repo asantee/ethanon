7875d558bd8a50c956a105c17c393c2b1d3e53ae
André Santee <andre.santee@gmail.com>
2023-01-18 18:24:06 -0300

    Indentation tweaks

--- toolkit/Source/src/gs2d/src/Math/GameMath.h
+++ toolkit/Source/src/gs2d/src/Math/GameMath.h
@@ -15,44 +15,45 @@ class Util
 public:
 	static float RadianToDegree(const float angle);
 	static float DegreeToRadian(const float angle);
-    static bool IsPowerOfTwo(const unsigned int n);
-    static unsigned int FindNextPowerOfTwoValue(const unsigned int n);
+	static bool IsPowerOfTwo(const unsigned int n);
+	static unsigned int FindNextPowerOfTwoValue(const unsigned int n);
+	
 };
 
 template <class T>
 T Max(const T &a, const T &b)
 {
-    return (a > b) ? a : b;
+	return (a > b) ? a : b;
 }
 
 template <class T>
 T Min(const T &a, const T &b)
 {
-    return (a < b) ? a : b;
+	return (a < b) ? a : b;
 }
 
 template <class T>
 T Abs(const T &n)
 {
-    return (n < 0) ? -n : n;
+	return (n < 0) ? -n : n;
 }
 
 template <class T>
 T Clamp(const T &n, const T &min, const T &max)
 {
-    return (n < min) ? min : (n > max) ? max : n;
+	return (n < min) ? min : (n > max) ? max : n;
 }
 
 template <class T>
 T Square(const T &a)
 {
-    return a*a;
+	return a*a;
 }
 
 template <class T>
 T Sign(const T &n)
 {
-    return (n < static_cast<T>(0)) ? static_cast<T>(-1) : (n > static_cast<T>(0)) ? static_cast<T>(1) : static_cast<T>(0);
+	return (n < static_cast<T>(0)) ? static_cast<T>(-1) : (n > static_cast<T>(0)) ? static_cast<T>(1) : static_cast<T>(0);
 }
 
 } // namespace math
